d54640426a093898943f2a7e068d1b9c70fd3b5a
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-06-24 02:17:48 +0800

    add handler,notes & some changes

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const initServer = async () => {
     const server = Hapi.server({
         port: 9000,
         host: 'localhost',
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Hello World!';
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
         },
     });
+    server.route(routes);
+
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
